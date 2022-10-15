7b61857f7fcaa534cfde844a5a226bd344a2c58b
Xxaosens <vlad.obito@mail.ru>
2022-10-15 17:58:48 +0300

    first commit

---
+++ JS/js.js
@@ -0,0 +1,127 @@
+
+let burger = document.querySelector(".burger__menu");
+burger.addEventListener("click", function () {
+   this.classList.toggle("active");
+})
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio");
+var isPlaying = false;
+
+function playMusic() {
+   isPlaying ? audio.pause() : audio.play();
+};
+
+audio.onplaying = function () {
+   isPlaying = true;
+};
+audio.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////GodsPlan///////////////////////////
+var myAudio = document.getElementById("audio1");
+var isPlaying = false;
+
+function playMusic1() {
+   isPlaying ? audio1.pause() : audio1.play();
+};
+
+audio1.onplaying = function () {
+   isPlaying = true;
+};
+audio1.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio2");
+var isPlaying = false;
+
+function playMusic2() {
+   isPlaying ? audio2.pause() : audio2.play();
+};
+
+audio2.onplaying = function () {
+   isPlaying = true;
+};
+audio2.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio3");
+var isPlaying = false;
+
+function playMusic3() {
+   isPlaying ? audio3.pause() : audio3.play();
+};
+
+audio3.onplaying = function () {
+   isPlaying = true;
+};
+audio3.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio4");
+var isPlaying = false;
+
+function playMusic4() {
+   isPlaying ? audio4.pause() : audio4.play();
+};
+
+audio4.onplaying = function () {
+   isPlaying = true;
+};
+audio4.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio5");
+var isPlaying = false;
+
+function playMusic5() {
+   isPlaying ? audio5.pause() : audio5.play();
+};
+
+audio5.onplaying = function () {
+   isPlaying = true;
+};
+audio5.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio6");
+var isPlaying = false;
+
+function playMusic6() {
+   isPlaying ? audio6.pause() : audio6.play();
+};
+
+audio6.onplaying = function () {
+   isPlaying = true;
+};
+audio6.onpause = function () {
+   isPlaying = false;
+};
+
+//////////////////////StarBoy///////////////////////////
+var myAudio = document.getElementById("audio7");
+var isPlaying = false;
+
+function playMusic7() {
+   isPlaying ? audio7.pause() : audio7.play();
+};
+
+audio7.onplaying = function () {
+   isPlaying = true;
+};
+audio7.onpause = function () {
+   isPlaying = false;
+};
+
+
